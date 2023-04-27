c4fa0e0bd0b83761d6bb44bedf83c504b71bb7a6
jeffgat <jeff.gtbn@gmail.com>
2023-04-27 14:34:21 -0600

    finished ch.2, guessing game

--- hello_cargo/src/main.rs
+++ hello_cargo/src/main.rs
@@ -1,13 +1,3 @@
-use std::io;
-
 fn main() {
-    println!("guess the number");
-
-    println!("please input your guess");
-
-    let mut guess = String::new();
-
-    io::stdin().read_line(&mut guess).expect("failed to read line");
-
-    println!("you guessed: {guess}")
+    println!("Hello, world!");
 }
