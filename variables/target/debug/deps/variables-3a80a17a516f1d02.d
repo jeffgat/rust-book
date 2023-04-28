92cd571e13b81f2d829377a46f0080065e133605
jeffgat <jeff.gtbn@gmail.com>
2023-04-28 16:35:26 -0600

    covered mutability and shadowing of variables

---
+++ variables/target/debug/deps/variables-3a80a17a516f1d02.d
@@ -0,0 +1,5 @@
+/Users/jeffreygatbonton/Desktop/Code/0-Practice/rust-book/variables/target/debug/deps/variables-3a80a17a516f1d02: src/main.rs
+
+/Users/jeffreygatbonton/Desktop/Code/0-Practice/rust-book/variables/target/debug/deps/variables-3a80a17a516f1d02.d: src/main.rs
+
+src/main.rs:
---
+++ variables/target/debug/variables.d
@@ -0,0 +1,1 @@
+/Users/jeffreygatbonton/Desktop/Code/0-Practice/rust-book/variables/target/debug/variables: /Users/jeffreygatbonton/Desktop/Code/0-Practice/rust-book/variables/src/main.rs
